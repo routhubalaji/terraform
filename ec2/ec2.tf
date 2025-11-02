b5b53b97ea0edf462658e41ae66bcf8ca74279ae
routhubalaji <routhubalaji111@gmail.com>
2025-11-02 07:05:45 +0530

    modified

--- ec2/ec2.tf
+++ ec2/ec2.tf
@@ -1,10 +1,10 @@
-resource "aws_instance" "terraform" {
+resource "aws_instance" "Terraform" {
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_all.id] 
 
   tags = {
-    Name = "terraform"
+    Name = "Terraform"
     Terraform = "true"
   }
 }
