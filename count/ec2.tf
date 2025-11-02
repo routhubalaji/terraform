0a77c55d96f34e86b30f392082755ef338cbfc88
routhubalaji <routhubalaji111@gmail.com>
2025-11-02 17:38:03 +0530

    r53records added

--- count/ec2.tf
+++ count/ec2.tf
@@ -1,5 +1,6 @@
 resource "aws_instance" "terraform" {
-  count=5
+  #count=5
+  count = length(var.instances)
   ami           = "ami-09c813fb71547fc4f"
   instance_type = "t3.micro"
   vpc_security_group_ids = [aws_security_group.allow_all.id] 
--- count/r53.tf
+++ count/r53.tf
@@ -1,5 +1,5 @@
 resource "aws_route53_record" "roboshop" {
-  count = 5
+  count = lenth(var.instances)
   zone_id = var.zone_id
   name    = "${var.instances[count.index]}.${var.domain_name}" # mongodb.daws86s.fun
   type    = "A"
