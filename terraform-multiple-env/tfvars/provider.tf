826ccf8825036dd71f06c3e79a991507281bd816
routhubalaji <routhubalaji111@gmail.com>
2025-11-07 23:15:18 +0530

    Multi Env creation

---
+++ terraform-multiple-env/tfvars/provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "6.16.0"
+    }
+  }
+
+  backend "s3" {
+  
+  }
+
+}
+
+provider "aws" {
+  # Configuration options
+  region = "us-east-1"
+}
