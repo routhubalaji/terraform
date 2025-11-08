e41f66d90ed61759240ab5e1a501005c0d256320
routhubalaji <routhubalaji111@gmail.com>
2025-11-08 11:40:46 +0530

    workspace

---
+++ terraform-multiple-env/workspace/locals.tf
@@ -0,0 +1,8 @@
+locals {
+  # env=lookup(var.environment,terraform.workspace)
+  common_name = "${var.project}-${terraform.workspace}"
+  common_tags = {
+    Project = var.project
+    Terraform = "true"
+  }
+}
